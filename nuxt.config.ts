66425fce9bdad86fb424a9d53fc97be3b07755fa
Saidjalol Turakhujayev <turakhujayevsaidjalol06@gmail.com>
2025-07-20 16:04:32 +0500

    bug fixed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     public: {
-      apiBase: 'https://oson-back-end.onrender.com/',
+      apiBase: 'https://oson-back-end.onrender.com',
     },
   },
 
